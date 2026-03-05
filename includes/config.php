54ee06562d424298bdc15086f7f7b5b9d00b0967
olasesi <ola.sesi@yahoo.com>
2026-03-05 10:46:23 +0100

    delete vendor, .env, etc.

--- includes/config.php
+++ includes/config.php
@@ -1,9 +1,10 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
-
-use Dotenv\Dotenv;
-
-$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
-
 session_start();
+
+define("BASE_URL", "http://localhost/whobaogofoundation/");
+define("DB_HOST", "localhost");
+define("DB_NAME", "whobaogo_whobaogofoundation");
+define("DB_USER", "whobaogo_wof_db_admin");
+define("DB_PASS", "cI5R]ooQ[esj)k-S");
+define("DB_PORT", 3306);
+define("API_KEY", "your_key_here");
--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,8 @@
 <?php
 try {
-    $dsn = "mysql:host={$_ENV['DB_HOST']};port={$_ENV['DB_PORT']};dbname={$_ENV['DB_NAME']};charset=utf8mb4";
+    $dsn = "mysql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME . ";charset=utf8mb4";
 
-    $pdo = new PDO($dsn, $_ENV['DB_USER'], $_ENV['DB_PASS'], [
+    $pdo = new PDO($dsn, DB_USER, DB_PASS, [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES   => false,
